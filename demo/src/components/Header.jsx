d386ecb59491bfe4b6f4d73a4b147c3f5396298b
alexander <xmedalex@yandex.ru>
2023-05-25 18:12:03 +0300

    init

--- demo/src/components/Header.jsx
+++ demo/src/components/Header.jsx
@@ -4,12 +4,23 @@ function Header() {
 	return (
 		<nav className='green darken-1'>
 			<div className='nav-wrapper'>
-				<a href='#' className='brand-logo'>
-					React Shop
+				<a
+					href='https://www.google.com'
+					className='brand-logo'
+					target='_blank'
+					rel='noreferrer'
+				>
+					React SPA
 				</a>
 				<ul id='nav-mobile' className='right hide-on-med-and-down'>
 					<li>
-						<a href='#'>Sass</a>
+						<a
+							href='https://www.google.com'
+							target='_blank'
+							rel='noreferrer'
+						>
+							Sass
+						</a>
 					</li>
 				</ul>
 			</div>
