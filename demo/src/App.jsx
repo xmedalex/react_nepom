43ec2dcb0ffa2158d7797799f0d1019aadba913e
alexander <xmedalex@yandex.ru>
2023-05-25 18:06:27 +0300

    init

--- demo/src/App.jsx
+++ demo/src/App.jsx
@@ -1,13 +1,12 @@
 import './App.css';
 import { Header } from './components/Header';
 import { Footer } from './components/Footer';
-import { Preloader } from './components/Preloader';
 
 function App() {
 	return (
 		<>
 			<Header />
-			<h1>New Project</h1>
+			<main className='container content'></main>
 			<Footer />
 		</>
 	);
